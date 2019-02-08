2dc700937546e3581b43718eaef60f4792efb5ac
Efrain Pinto <efrapp@gmail.com>
2019-02-08 10:12:18 -0500

    Adding validation to the Bubble Sort code

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,21 +1,24 @@
 def bubble_sort_by array
-    for j in 0..array.length
-        for i in 0...array.length-1
-            val = yield array[i], array[i + 1]
-            
-            if val > 0
-                temp = array[i]
-                array[i] = array[i+1]
-                array[i+1] = temp
+    if block_given?
+        for j in 0..array.length
+            for i in 0...array.length-1        
+                val = yield array[i], array[i + 1]
+                
+                if val > 0
+                    temp = array[i]
+                    array[i] = array[i+1]
+                    array[i+1] = temp
+                end
             end
         end
+        array
+    else
+        "Please provide the block for the sorting criteria"
     end
-
-    return array
 end
 
 result = bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
-    right.length - left.length
+    left.length - right.length
 end
 
-print result
+p result
