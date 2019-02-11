becacdb66d4eceff018b6b684c6c92ea091d5e46
Efrain Pinto <efrapp@gmail.com>
2019-02-11 08:04:28 -0500

    Creating custom function for some Enumerator methods and adding file for test them

--- Enumerator.rb
+++ Enumerator.rb
@@ -8,10 +8,10 @@ module Enumerable
     end
 
     def my_each_with_index
-     i = 0
-      while i < self.length
-  		yield(self[i],i)
-      	i+=1     	
+			i = 0
+      while i < self.length do
+  			yield(self[i],i)
+      	i += 1     	
       end
     end
 
@@ -20,7 +20,7 @@ module Enumerable
     	self.my_each do|element| 
     		if yield(element)
     			 selected.push(element)
-    	    end
+				end
     	end
         selected
     end
@@ -76,6 +76,43 @@ module Enumerable
 			return self.length
 		end
 	end
+
+	def my_map proc = nil
+		new_val = nil
+		new_arr = []
+
+		if !proc
+			self.my_each do |element|
+				new_val = yield element
+				new_arr << new_val
+			end
+		else
+			self.my_each do |element|
+				new_val = proc.call element
+				new_arr << new_val
+			end
+		end
+
+		new_arr
+	end
+
+	def my_inject initial = nil
+		
+		if initial
+			first_val = initial
+			i = 0
+		else
+			first_val = self[0]
+			i = 1
+		end
+
+		while i < self.length
+			accum = yield first_val, self[i]
+			first_val = accum
+			i += 1
+		end
+
+		accum
+	end
+
 end
-  some =  [0,0,0,1].my_count{|x| x > 0}
-  puts some
